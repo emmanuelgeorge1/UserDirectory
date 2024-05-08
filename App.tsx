24bd0e64bf0756e1fc847acb0b0095b39a29b6c4
ronymep <ronymep@gmail.com>
2024-05-09 01:13:27 +0530

    Updated App.tsx to use DrawerNavigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import DrawerNavigator from './navigation/DrawerNavigatior';
+import { makeServer } from '../mocks/server';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer({ environment: "development" });
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DrawerNavigator/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
