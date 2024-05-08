a800245c2187e81ef42e6fc1d4979d798f7c87e9
ronymep <ronymep@gmail.com>
2024-05-09 01:21:35 +0530

    Added constants for texts and colors

---
+++ app/constants/Constants.ts
@@ -0,0 +1,16 @@
+export const TEXTS = {
+    USER_TYPES: 'User Types',
+    ADMIN: 'Admin',
+    MANAGER: 'Manager',
+    MANAGE_USERS: 'Manage Users',
+    EMPTY_SCREEN: 'Empty Screen',
+    SEARCH_USERS: 'Search Users...',
+  };
+  export const COLORS = {
+    WHITE: '#fff',
+    BLACK: 'black',
+    LIGHT_GREY: 'lightgrey',
+    DARK_GREY: 'gray',
+    LIGHT_BLUE: '#bbe2ff',
+    DARK_BLUE: '#4070FF',
+  };
